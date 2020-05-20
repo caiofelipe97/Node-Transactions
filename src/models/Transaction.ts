78f18089d3dbbf9f48737a3e230e6e8ca2ab6733
Caio Melo <caiofelipee17@gmail.com>
2020-05-20 18:40:58 -0300

    Add POST /transactions route

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,39 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
+  @Column({ name: 'category_id' })
   category_id: string;
 
+  @OneToOne(() => Category)
+  @JoinColumn()
+  category: string;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,52 @@
 // import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const categoriesRepository = getRepository(Category);
+
+    let transactionCategory = await categoriesRepository.findOne({
+      title: category,
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      transactionCategory = await categoriesRepository.save(
+        transactionCategory,
+      );
+    }
+
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: transactionCategory.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
