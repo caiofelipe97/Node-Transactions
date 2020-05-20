0abd527009474c044ec7f15aabb21e1e53b24f2e
Caio Melo <caiofelipee17@gmail.com>
2020-05-20 18:53:35 -0300

    Add GET /transactions route

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,24 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const income = transactions
+      .filter((transaction: Transaction) => transaction.type === 'income')
+      .map((transaction: Transaction) => transaction.value)
+      .reduce((acc, value) => {
+        const accumulator = acc + value;
+        return accumulator;
+      }, 0);
+    const outcome = transactions
+      .filter((transaction: Transaction) => transaction.type === 'outcome')
+      .map((transaction: Transaction) => transaction.value)
+      .reduce((acc, value) => {
+        const accumulator = acc + value;
+        return accumulator;
+      }, 0);
+    const total = income - outcome;
+    const balance = { income, outcome, total };
+    return balance;
   }
 }
 
